a626801e74c8e24591954bd7a04ff08344cd2f09
bdalrhmnaymnlasbh849-star <bdalrhmnaymnlasbh849@gmail.com>
2026-07-17 21:27:47 +0300

    init OOP

--- main.cpp
+++ main.cpp
@@ -3,36 +3,46 @@
 
 using namespace std;
 
-float CalculateSalaryForDoctor(float HoursTime, float SalaryPerHour)
+class Weapon
 {
-    float salary = HoursTime * SalaryPerHour * 1.25;
-    return salary;
-}
+public:
+    int capacity;
+    float fireRate;
 
-float CalculateSalaryForNurse(float HoursTime, float SalaryPerHour)
-{
-    float salary = HoursTime * SalaryPerHour * 1.15;
-    return salary;
-}
+    float Fire()
+    {
+        return capacity * fireRate;
+    }
 
-float SubstractFromDoctor(float HoursTime, float SalaryPerHour)
-{
-    float result = HoursTime * SalaryPerHour * 1.3; 
-}
+    float Reload()
+    {
+        return capacity / fireRate;
+    }
+};
 
-float SubstractFromNurse(float HoursTime, float SalaryPerHour)
+class Assassin : public Weapon
 {
-    float result = HoursTime * SalaryPerHour * 1.5; 
-}
+};
+
+class Sniper : public Weapon
+{
+public:
+    int scope;
+
+    float Fire()
+    {
+        float result = Weapon::Fire();
+        return (result * scope);
+    }
+};
+
+
 
 int main()
 {
-    string doctorName = "Mansour";
-    float salaryPerHour = 135.5;
-    float hoursTime = 50.25;
-
-    float result = CalculateSalaryForDoctor(hoursTime, salaryPerHour);
-    cout << "Salary For Doctor " << doctorName << " is: " << result << endl;
-
-    cout << "Bye";
+    Sniper Aum;
+    Aum.capacity = 7;
+    Aum.fireRate = 120;
+    Aum.scope = 8;
+    cout << Aum.Fire();
 }
